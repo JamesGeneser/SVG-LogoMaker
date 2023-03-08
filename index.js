c1eec026a97dc1eb6833e32e1e5596c768bd929b
James Geneser <jamesgeneser@gmail.com>
2023-03-08 16:11:03 -0700

    sends data to shapes.js

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 const inquirer = require("inquirer");
+const { text } = require("stream/consumers");
 const shapes = require("./lib/shapes.js");
 
 const getUserChoices = () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Enter up to three characters for your logo",
-        name: "text",
-      },
-      {
-        type: "input",
-        message: "What color do you want your text to be?",
-        name: "text-color",
-      },
-      {
-        type: "list",
-        message: "What shape do you want to represent your logo",
-        name: "shape",
-        choices: ["circle", "triangle", "square"],
-      },
-      {
-        type: "input",
-        message: "What color do you want your shape to be?",
-        name: "shape-color",
-      },
-    ])
-    .then((response) => console.log(response));
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter up to three characters for your logo",
+      name: "text",
+    },
+    {
+      type: "input",
+      message: "What color do you want your text to be?",
+      name: "textColor",
+    },
+    {
+      type: "list",
+      message: "What shape do you want to represent your logo",
+      name: "shape",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      message: "What color do you want your shape to be?",
+      name: "shapeColor",
+    },
+  ]);
+  // .then((response) => console.log(response));
 };
 
-const generateSvg = ({ text, textColor, shape, shapeColor }) => {
-  return `${text},${textColor},${shape},${shapeColor}`;
-};
+// const generateSvg = ({ text, textColor, shape, shapeColor }) => {
+//   return `${text},${textColor},${shape},${shapeColor}`;
+// };
 const init = () => {
   getUserChoices().then((answers) => {
-    switch (answers.shape) {
-    }
+    console.log(answers);
+    const generateLogo = new shapes(answers);
+    generateLogo;
   });
 };
+init();
